5fe8d216bed6ec553c56e5ba2f74c4f0185f690d
mec391 <56007839+mec391@users.noreply.github.com>
2021-02-13 00:27:55 -0500

    Add files via upload

--- assignment_3_tb.v
+++ assignment_3_tb.v
@@ -28,28 +28,31 @@ cnt_inputs = 0;
 
 //Priority Encoder should keep same value for 2 time delays
 //ones counter should count up every 1 time delay
-#1 enc_inputs = 8'b1000_0000; cnt_inputs = 8'b0000_0001;
-#1 enc_inputs = 8'b1111_1111; cnt_inputs = 8'b0000_0011;
-#1 enc_inputs = 8'b0100_0000; cnt_inputs = 8'b0000_0111;
-#1 enc_inputs = 8'b0111_1111; cnt_inputs = 8'b0000_1111;
-#1 enc_inputs = 8'b0010_0000; cnt_inputs = 8'b0001_1111;
-#1 enc_inputs = 8'b0011_1111; cnt_inputs = 8'b0011_1111;
-#1 enc_inputs = 8'b0001_0000; cnt_inputs = 8'b0111_1111;
-#1 enc_inputs = 8'b0001_1111; cnt_inputs = 8'b1111_1111;
-#1 enc_inputs = 8'b0000_1000; cnt_inputs = 8'b1111_1110;
-#1 enc_inputs = 8'b0000_1111; cnt_inputs = 8'b1111_1100;
-#1 enc_inputs = 8'b0000_0100; cnt_inputs = 8'b1111_1000;
-#1 enc_inputs = 8'b0000_0111; cnt_inputs = 8'b1111_0000;
-#1 enc_inputs = 8'b0000_0010; cnt_inputs = 8'b1110_0000;
-#1 enc_inputs = 8'b0000_0011; cnt_inputs = 8'b1100_0000;
-#1 enc_inputs = 8'b0000_0001; cnt_inputs = 8'b1000_0000;
-#1 enc_inputs = 0;				cnt_inputs = 0;
+#1 enc_inputs = 8'b1000_0000; //cnt_inputs = 8'b0000_0001;
+#1 enc_inputs = 8'b1111_1111; //cnt_inputs = 8'b0000_0011;
+#1 enc_inputs = 8'b0100_0000; //cnt_inputs = 8'b0000_0111;
+#1 enc_inputs = 8'b0111_1111; //cnt_inputs = 8'b0000_1111;
+#1 enc_inputs = 8'b0010_0000; //cnt_inputs = 8'b0001_1111;
+#1 enc_inputs = 8'b0011_1111; //cnt_inputs = 8'b0011_1111;
+#1 enc_inputs = 8'b0001_0000; //cnt_inputs = 8'b0111_1111;
+#1 enc_inputs = 8'b0001_1111; //cnt_inputs = 8'b1111_1111;
+#1 enc_inputs = 8'b0000_1000; //cnt_inputs = 8'b1111_1110;
+#1 enc_inputs = 8'b0000_1111; //cnt_inputs = 8'b1111_1100;
+#1 enc_inputs = 8'b0000_0100; //cnt_inputs = 8'b1111_1000;
+#1 enc_inputs = 8'b0000_0111; //cnt_inputs = 8'b1111_0000;
+#1 enc_inputs = 8'b0000_0010; //cnt_inputs = 8'b1110_0000;
+#1 enc_inputs = 8'b0000_0011; //cnt_inputs = 8'b1100_0000;
+#1 enc_inputs = 8'b0000_0001; //cnt_inputs = 8'b1000_0000;
+#1 enc_inputs = 0;				//cnt_inputs = 0;
 //this should trigger a 1 for all_zero
 #1 enc_inputs = 0;
 
-
 end
 
+always begin //allows for all input values for the ones counter
+#1 cnt_inputs = cnt_inputs + 1;
+
+end
 
 
 //generate monitor
